21ab173066a6e4066a8408a00c87ce12456b3625
Kjetil Haugland <haugkjet@gmail.com>
2022-07-01 09:52:41 +0200

    Adden some stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import Image from "next/image";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
